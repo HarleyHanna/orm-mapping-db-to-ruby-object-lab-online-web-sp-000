083ed3667c3779ab997433d2cfc06d10300ccd1a
Harley Hanna <harleyhannawrit@gmail.com>
2020-08-31 23:46:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,6 +17,14 @@ class Student
     
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+  end
 
   def self.find_by_name(name)
     sql = <<-SQL
